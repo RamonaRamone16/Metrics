7da8b9ed42afba17ff9ecbf83726cccd4ec36490
Ramona Ramone <ramonaramonr@gmail.com>
2021-09-29 17:00:04 +0600

    Added create form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import './style/App.css';
 import { UserActivityTable } from "./components/UserActivityTable";
+import { MyButton } from './components/UI/button/MyButton';
+import { UserActivityForm } from './components/UserActivityForm';
+import { useState } from 'react';
 
 const App = () => {
-  const usersActivities = [
+  const [usersActivities, setUsersActivities] = useState([ 
     {id: 1, dateRegistration: '2021-09-01', dateLastActivity: '2021-09-20'},
     {id: 1, dateRegistration: '2021-09-03', dateLastActivity: '2021-09-04'},
     {id: 2, dateRegistration: '2021-09-15', dateLastActivity: '2021-09-27'}
-  ];
+  ]);
+  const [form, setForm] = useState(false);
+
+  const createUserActivity = (newUserActivity) => {
+    setUsersActivities([...usersActivities, newUserActivity]);
+  }
 
   return (
-    <UserActivityTable usersActivities={usersActivities}/>
+    <div>
+      <MyButton onclick={() => setForm(true)}/>
+      <UserActivityTable usersActivities={usersActivities}/>
+      <UserActivityForm visible={form} setVisible={setForm} create={createUserActivity}/>
+    </div>
   )
 }
 export default App;
---
+++ src/components/UserActivityForm.js
@@ -0,0 +1,36 @@
+import { React, useState } from 'react';
+import { MyButton } from './UI/button/MyButton';
+import { MyInput } from './UI/input/MyInput';
+
+export const UserActivityForm = ({create, visible, setVisible}) => {
+  const [userActivity, setUserActivity] = useState({id: '', dateRegistration: '', dateLastActivity: ''});
+
+  const createUserActivity = () => {
+    create(userActivity);
+    setUserActivity({id: '', dateRegistration: '', dateLastActivity: ''});
+  }
+
+  return (
+    <form>
+      <MyInput
+      value={userActivity.id}
+      onChange={e => setUserActivity({...userActivity, id: e.target.value})}
+      type="input"
+      placeholder="type user id"
+      />
+      <MyInput
+      value={userActivity.dateRegistration}
+      onChange={e => setUserActivity({...userActivity, dateRegistration: e.target.value})}
+      type="input"
+      placeholder="type user registration date"
+      />
+      <MyInput
+      value={userActivity.dateLastActivity}
+      onChange={e => setUserActivity({...userActivity, dateLastActivity: e.target.value})}
+      type="input"
+      placeholder="type user last activity date"
+      />
+      <MyButton onClick={createUserActivity}>Create</MyButton>
+    </form>
+  )
+}
